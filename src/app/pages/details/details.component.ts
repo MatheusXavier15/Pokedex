100f893ff902b25dc896c4935f78ab10ef224b7a
Matheus Xavier <matheusxavier055@gmail.com>
2022-09-02 01:03:55 -0300

    Pokemon details / Error handling

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/services/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlPokeName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
-  constructor() { }
+  public pokemon: any;
+  public isLoading: boolean = true;
+  public apiError: boolean = false;
+
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
 
   ngOnInit(): void {
+    this.getPokemon;
   }
 
+  get getPokemon() {
+    const id = this.activatedRouter.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.pokeApiService.apiGetPokemons(
+      `${this.urlPokeName}/${id}`
+    );
+
+    return forkJoin([pokemon, name]).subscribe({
+      next: (response) => {
+        this.pokemon = response;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.apiError = true;
+      },
+    });
+  }
 }
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -4,28 +4,32 @@ import { PokeApiService } from 'src/app/services/poke-api.service';
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
-  styleUrls: ['./poke-list.component.scss']
+  styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-
   private setAllPokemons: any;
   public getAllPokemons: any;
+  public apiError: boolean = false;
 
-  constructor(private pokeApiService: PokeApiService) { }
+  constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe(
-      res => {
-        this.setAllPokemons = res.results
-        this.getAllPokemons = this.setAllPokemons
-      }
-    );
+    this.pokeApiService.apiListAllPokemons.subscribe({
+      next: (res) => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+
+      },
+      error: (error) => {
+        this.apiError = true;
+      },
+    });
   }
 
-  public getSearch(value: string){
+  public getSearch(value: string) {
     const filter = this.setAllPokemons.filter((pokemon: any) => {
-      return !pokemon.name.indexOf(value.toLowerCase())
-    })
+      return !pokemon.name.indexOf(value.toLowerCase());
+    });
 
     this.getAllPokemons = filter;
   }
